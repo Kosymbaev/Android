ba1f201d69b59d596fcac73ababa084c0db51f94
Kosymbaev <kosymbaevalex@mail.ru>
2023-08-06 18:11:32 +0300

    Start page with 2 buttons: submit and close app, checkbox, 2 labels, 2 entrys

---
+++ App1/App1/StartPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    public partial class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await DisplayAlert("Registrate","You create accont","OK");
+        }
+    }
+}
